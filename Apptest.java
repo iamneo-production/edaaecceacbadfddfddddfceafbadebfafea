c20ba7fadb33e42fa52736039638c5473c7777f0
Iamneo <techadmin@iamneo.ai>
2023-09-01 06:49:53 +0000

    Backup Commit

--- Apptest.java
+++ Apptest.java
@@ -7,35 +7,35 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
-public class Apptest { 
-ChromeDriver driver;
-String url = "http://www.ebay.in";
-public void invokeBrowser() {
-System.setProperty("webdriver.chrome.driver",
-"C:\\Users\\Ritika Bharadwaj\\workspace\\libs\\chromeDriver36\\chromedriver.exe"); 
-driver = new ChromeDriver();
-driver.manage().window().maximize();
-driver.manage().deleteAllCookies();
-driver.manage().timeouts().pageLoadTimeout(90, TimeUnit.SECONDS);
-driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
-driver.get(url);
-}
-public void searchProduct(String product, String category) {
-driver.findElement(By.id("gh-ac")).sendKeys(product);
-WebElement dropdown = driver.findElement(By.id("gh-cat"));
-Select selectCategory = new Select(dropdown);
-selectCategory.selectByVisibleText(category);
-driver.findElement(By.id("gh-btn")).click();
-String result = driver.findElement(By.className("listingscnt")).getText();
-System.out.println("Result :: " + result);
-}
-public void getNthProduct(int itemNumber) {
-String productXpath = String.format("//div[@id='ResultSetItems']/ul[@id='ListViewInner']/li[%d]", itemNumber);
-String nthProduct = driver.findElement(By.xpath(productXpath)).getText();
-System.out.println("Nth Product :: " + nthProduct);
-}
-public void getAllProducts() {
-List<WebElement> allProduct = driver.findElements(By.xpath("//div[@id='ResultSetItems']/ul[@id='ListViewInner']/li"));
+public class Apptest { // Changed class name to Apptest
+    ChromeDriver driver;
+    String url = "http://www.ebay.in";
+    public void invokeBrowser() {
+      System.setProperty("webdriver.chrome.driver",
+          "C:\\Users\\Ritika\\workspace\\libs\\chromeDriver36\\chromedriver.exe"); // Updated name
+      driver = new ChromeDriver();
+      driver.manage().window().maximize();
+      driver.manage().deleteAllCookies();
+      driver.manage().timeouts().pageLoadTimeout(90, TimeUnit.SECONDS);
+      driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+      driver.get(url);
+    }
+    public void searchProduct(String product, String category) {
+      driver.findElement(By.id("gh-ac")).sendKeys(product);
+      WebElement dropdown = driver.findElement(By.id("gh-cat"));
+      Select selectCategory = new Select(dropdown);
+      selectCategory.selectByVisibleText(category);
+      driver.findElement(By.id("gh-btn")).click();
+      String result = driver.findElement(By.className("listingscnt")).getText();
+      System.out.println("Result :: " + result);
+    }
+    public void getNthProduct(int itemNumber) {
+      String productXpath = String.format("//div[@id='ResultSetItems']/ul[@id='ListViewInner']/li[%d]", itemNumber);
+      String nthProduct = driver.findElement(By.xpath(productXpath)).getText();
+      System.out.println("Nth Product :: " + nthProduct);
+    }
+    public void getAllProducts() {
+      List<WebElement> allProduct = driver.findElements(By.xpath("//div[@id='ResultSetItems']/ul[@id='ListViewInner']/li"));
 for (WebElement product : allProduct) {
 System.out.println(product.getText());
 System.out.println("-----------------------------------------");
@@ -68,4 +68,3 @@ jsEngine = (JavascriptExecutor) driver;
 jsEngine.executeScript(jsCommand);
 }
 }
-
